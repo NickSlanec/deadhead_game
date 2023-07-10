dcea16a0d4125fff79429ca9c506cd99466ded15
Nick Slanec <44660208+NickSlanec@users.noreply.github.com>
2023-07-09 19:36:04 -0700

    added chalice REST

--- deadhead_chalice/app.py
+++ deadhead_chalice/app.py
@@ -1,25 +1,21 @@
 from chalice import Chalice
 import requests
-import pandas as pd
 import random
-
 
 app = Chalice(app_name='deadhead_chalice')
 
-app.debug = True
 
-@app.route("/concert", cors=True)
+@app.route("/concert")
 def get_concert():
   r = requests.get("https://archive.org/advancedsearch.php?q=collection%3A%22GratefulDead%22&output=json")
   data = r.json()['response']['docs']
-  df = pd.DataFrame(data)
-  concert = df.sample()
-  return(concert.to_json(orient='records'))
+  return(random.choice(data))
 
-@app.route("/song", cors=True)
+@app.route("/song")
 def get_song():
     request = app.current_request
-    concert_url = request.args.get('concert')
+    body = request.json_body
+    concert_url = body['concert']
     url = "https://archive.org/details/{}".format(concert_url)
     songs = requests.get(url+'?output=json')
     songjson = songs.json()
